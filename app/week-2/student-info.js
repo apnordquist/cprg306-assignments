c0122da64f8e0b4678add2594adf5b2c38f84ca8
Adam Nordquist <apnordquist@gmail.com>
2024-09-16 13:21:09 -0600

    Week 2
    Completed the week 2 assignment to create a new page and a new component.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo()
+{
+    return (
+        <main>
+            <p>Adam Nordquist</p>
+            <Link href="https://github.com/apnordquist/cprg306-assignments">https://github.com/apnordquist/cprg306-assignments</Link>
+        </main>
+    );
+}
